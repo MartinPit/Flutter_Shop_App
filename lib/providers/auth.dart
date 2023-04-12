f1f5c806ef12a271740c1ee837079ee8966b6c1b
Martin Oliver Pitonak <martin.pit18@gmail.com>
2023-04-12 19:20:54 +0200

    Properly managed env variables

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
+import 'package:my_shop/env/env.dart';
 
 class Auth with ChangeNotifier {
   String? _token;
@@ -10,7 +11,7 @@ class Auth with ChangeNotifier {
 
   Future<void> signup(String email, String pass) async {
     final url = Uri.https("identitytoolkit.googleapis.com",
-        "/v1/accounts:signUp?key=[API_KEY]");
+        "/v1/accounts:signUp?key=${Env.apiKey}");
     final response = await post(url,
         body: json.encode(
             {'email': email, 'password': pass, 'returnSecureToken': true}));
